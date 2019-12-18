115f5f9783fa53dc5d99552282a629f8eb1cec41
JunhoYeo <32605822+JunhoYeo@users.noreply.github.com>
2019-12-19 08:10:46 +0900

    feat: illusts in Developer job page

--- packages/recurit/src/pages/jobs/Developer.tsx
+++ packages/recurit/src/pages/jobs/Developer.tsx
@@ -1,3 +1,4 @@
+import raw from 'raw.macro';
 import React, { Fragment } from 'react';
 import TextLoop from 'react-text-loop';
 import styled from 'styled-components';
@@ -6,15 +7,46 @@ import Header from '../../components/Header';
 import Layout from '../../components/Layout';
 import Markdown from '../../components/Markdown';
 
-import raw from 'raw.macro';
-
 import developerTitles from '../../data/developer/titles.json';
+
+import developerIllust from '../../assets/illusts/dev-4.png';
+import lightbulbIllust from '../../assets/illusts/lightbulb.png';
 
 const CulturePost = raw('../../data/developer/culture.md');
 
 const HeaderWrap = styled.div`
   height: 60vh;
   position: relative;
+`;
+
+const IllustWrap = styled.div`
+  position: absolute;
+  right: 0;
+  width: 62%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+`;
+
+const IllustContainer = styled.div`
+  position: relative;
+  width: 500px;
+  height: 100%;
+`;
+
+const DeveloperIllust = styled.img`
+  height: 436px;
+  position: absolute;
+  bottom: 0;
+  right: 156px;
+`;
+
+const LightbulbIllust = styled.img`
+  height: 145px;
+  position: absolute;
+  right: 58px;
+  top: 36px;
+  transform: rotate(28deg);
 `;
 
 const StyledHeader = styled(Header)`
@@ -56,6 +88,12 @@ export default class DeveloperJob extends React.Component {
     return (
       <Layout className="developer-recurit">
         <HeaderWrap>
+          <IllustWrap>
+            <IllustContainer>
+              <DeveloperIllust src={developerIllust} />
+              <LightbulbIllust src={lightbulbIllust} />
+            </IllustContainer>
+          </IllustWrap>
           <StyledHeader
             title={<HeaderTitle />}
             desc="열정만 있다면, 최고로 만들어 드리겠습니다."
