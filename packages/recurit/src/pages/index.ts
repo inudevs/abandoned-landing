b7b2caf4b707cfafdf36717713be15f5dd78896c
JunhoYeo <32605822+JunhoYeo@users.noreply.github.com>
2020-01-04 16:52:49 +0900

    refactor: code splitting for pages

--- packages/recurit/src/pages/index.ts
+++ packages/recurit/src/pages/index.ts
@@ -1,6 +1,9 @@
-export { default as About } from './About';
-export { default as Course } from './Course';
-export { default as Home } from './Home';
-export {
-  JobDeveloper,
-} from './jobs';
+import loadable from '@loadable/component';
+
+export const About = loadable(() => import('./About'));
+export const Course = loadable(() => import('./Course'));
+export const Home = loadable(() => import('./Home'));
+
+export const JobDesigner = loadable(() => import('./jobs/Designer'));
+export const JobDeveloper = loadable(() => import('./jobs/Developer'));
+export const JobMarketer = loadable(() => import('./jobs/Marketer'));
--- packages/recurit/src/pages/jobs/index.ts
+++
@@ -1,3 +0,0 @@
-export { default as JobDesigner } from './Designer';
-export { default as JobDeveloper } from './Developer';
-export { default as JobMarketer } from './Marketer';
