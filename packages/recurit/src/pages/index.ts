136fe7c6bd6138e928f0d1f5c8b2051da530cc0f
JunhoYeo <32605822+JunhoYeo@users.noreply.github.com>
2020-01-04 20:58:42 +0900

    feat: add LayoutWithTheme component

--- packages/recurit/src/pages/index.ts
+++ packages/recurit/src/pages/index.ts
@@ -3,6 +3,7 @@ import loadable from '@loadable/component';
 export const About = loadable(() => import('./About'));
 export const Course = loadable(() => import('./Course'));
 export const Home = loadable(() => import('./Home'));
+export const Job = loadable(() => import('./Job'));
 
 export const JobDesigner = loadable(() => import('./jobs/Designer'));
 export const JobDeveloper = loadable(() => import('./jobs/Developer'));
--- packages/recurit/src/pages/jobs/Marketer.tsx
+++ packages/recurit/src/pages/jobs/Marketer.tsx
@@ -1,10 +1,10 @@
 import * as React from 'react';
 
-import Layout from '../../components/atoms/Layout';
+import LayoutWithTheme from '../../components/molecules/LayoutWithTheme';
 
 const BranderJob: React.FC = () => {
   return (
-    <Layout
+    <LayoutWithTheme
       className="brander-recurit"
     />
   );
